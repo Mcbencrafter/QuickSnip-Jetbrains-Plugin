774a6f32aaca0b39c7cf48e880b1fd88a8599326
Mcbencrafter <mcbencrafter01@gmail.com>
2025-01-07 17:28:29 +0100

    setup

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("java")
+    id("org.jetbrains.intellij") version "1.17.4"
+}
+
+group = "net.mcbencrafter"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+intellij {
+    version.set("2024.1.7")
+    type.set("IC")
+
+    plugins.set(listOf())
+}
+
+tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("241")
+        untilBuild.set("243.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
